03c179176199f65b2563cf76ea43e8589ebee0a4
taajuddin <taajraza@gmail.com>
2020-08-17 17:30:51 +0530

    Updates

---
+++ precache-manifest.1cf910fed8207f2188f812da1e620a67.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4512bd643035512eda7ae65ac754c0b",
+    "url": "/blog-app/index.html"
+  },
+  {
+    "revision": "21dde0bda2e2e691a15d",
+    "url": "/blog-app/static/css/2.f14b8c5e.chunk.css"
+  },
+  {
+    "revision": "eeae1f6c1fff4dc10b0d",
+    "url": "/blog-app/static/css/main.547b187d.chunk.css"
+  },
+  {
+    "revision": "21dde0bda2e2e691a15d",
+    "url": "/blog-app/static/js/2.24edad74.chunk.js"
+  },
+  {
+    "revision": "eeae1f6c1fff4dc10b0d",
+    "url": "/blog-app/static/js/main.96dae115.chunk.js"
+  },
+  {
+    "revision": "07581233709cc84b8f2f",
+    "url": "/blog-app/static/js/runtime~main.7e17012d.js"
+  },
+  {
+    "revision": "f78f951d3bebe71dc84a8fca749a91cd",
+    "url": "/blog-app/static/media/post.f78f951d.jpg"
+  }
+]);
